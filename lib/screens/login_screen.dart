374e46d46d693f0eea4dd0145c60d49bcd8e8be8
Hamster <ray@ketagalan.com>
2026-06-12 00:08:59 +0800

    Prompt and persist ToS acceptance on login
    Make LoginScreen stateful and add an initState check that compares the stored 'accepted_terms_version' (SharedPreferences) with the current app version (package_info_plus). If versions differ, push TermsOfServiceScreen and, if the user agrees, persist the current version. Add imports for shared_preferences, package_info_plus, and terms_of_service_screen. Also remove the editable "remember me" checkbox in the login form (fixing it to true) and adjust surrounding spacing.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'terms_of_service_screen.dart';
 import 'login_form.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _checkTermsAgreement();
+    });
+  }
+
+  Future<void> _checkTermsAgreement() async {
+    final prefs = await SharedPreferences.getInstance();
+    final info = await PackageInfo.fromPlatform();
+    final currentVersion = info.version;
+
+    final String lastAcceptedVersion =
+        prefs.getString('accepted_terms_version') ?? '';
+
+    if (lastAcceptedVersion != currentVersion) {
+      if (mounted) {
+        final bool? agreed = await Navigator.push<bool>(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                const TermsOfServiceScreen(showAgreementButtons: true),
+          ),
+        );
+
+        if (agreed == true) {
+          await prefs.setString('accepted_terms_version', currentVersion);
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
